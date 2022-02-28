b9ce3cb24abfac5e8b9369e7c7273654710e9e5d
Quentin Lagache <qlagache@gmail.com>
2022-02-28 17:42:11 +0100

    test path

--- routes/question.js
+++ routes/question.js
@@ -140,8 +140,8 @@ router.post("/question/publish", isAuthenticated, async (req, res) => {
             } else {
                 // Envoi de l'image à cloudinary
                 const result = await cloudinary.uploader.upload(
-                    req.files.questionPicture.path
-                    // "cep_upload",
+                    req.files.questionPicture.path,
+                    "cep_upload"
                     // {
                     //     // folder: `CultureEnPoche/questionPicture/${newQuestion._id}`,
                     //     folder: `/${newQuestion._id}`,
