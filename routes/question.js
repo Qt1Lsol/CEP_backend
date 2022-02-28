8f4e91255114da9d913b85fe450e3233923f9a1c
Quentin Lagache <qlagache@gmail.com>
2022-02-28 10:34:56 +0100

    test

--- routes/question.js
+++ routes/question.js
@@ -107,7 +107,7 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 // route qui permet de poster une nouvelle annonce
 // router.post("/question/publish", isAuthenticated, async (req, res) => {
 
-router.post("/question/publish", isAuthenticated, async (req, res) => {
+router.post("/question/publish", async (req, res) => {
 
     // console.log(req.fields);
 
