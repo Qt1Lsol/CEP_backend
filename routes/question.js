e41dc6f918157a0f66ebbfbbc6a3fcfa753b7de4
Quentin Lagache <qlagache@gmail.com>
2022-03-02 11:20:19 +0100

    QV4

--- routes/question.js
+++ routes/question.js
@@ -105,7 +105,7 @@ const isAuthenticated = require("../middleware/isAuthenticated");
 
 // get all question of an Author
 router.get("question/view", async (req, res) => {
-    res.json("message:ok");
+    res.status(400).json({ message: "OK" });
 //   try {
 //     // const question = await Question.find({author: req.query.author})
 //     const question = await Question.find()
